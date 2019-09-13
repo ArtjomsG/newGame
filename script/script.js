8e4297bf63c520763fc68f07c5f95a102457e285
ArtjomsG <artjoms.markovins.1994@gmail.com>
2019-09-13 14:50:40 -0500

    Updates 09/12/2019

--- script/script.js
+++ script/script.js
@@ -1,86 +1,92 @@
 //Make an player objects for dog an princess//
 var dog = {
-    playerType: 'dog',
-    playerImage: '<img src="img/dog.png" alt="dog">',
-    playerPos: {
-        col: 0,
-        row: 0
-    },
-    weapon: 'donut',
+  playerType: 'dog',
+  playerImage: '<img src="img/dog.png" alt="dog">',
+  playerPos: {
+      col: 0,
+      row: 0
+  },
+  weapon: {
+    weaponName: 'donut',
     //weaponImage: '<img src="img/donut.jpg" alt="donut">',
     weaponImage: './img/donut.jpg',
     damage: 10,
-    playerHealth: 100
+
+  },
+  playerHealth: 100
 };
 var princess = {
-    playerType: 'princess',
-    playerImage: '<img src="img/princes.png" alt="princess">',
-    playerPos: {
-        col: 0,
-        row: 0
-    },
-    weapon: 'donut',
+  playerType: 'princess',
+  playerImage: '<img src="img/princes.png" alt="princess">',
+  playerPos: {
+      col: 0,
+      row: 0
+  },
+  weapon: {
+    weaponName: 'donut',
     //weaponImage: '<img src="img/donut.jpg" alt="donut">',
     weaponImage: './img/donut.jpg',
     damage: 10,
-    playerHealth: 100
+
+  },
+  playerHealth: 100
 };
 var activePlayer = dog;
-var activeWeapon = activePlayer;
+var activeWeapon = activePlayer.weapon;
 //Function to generate random number//
 function randomRowColumnNumber(max){
-    const number= Math.floor(Math.random() * max) + 1;
-    return number;
+  const number= Math.floor(Math.random() * max) + 1;
+  return number;
 };
 
 //WEAPON Objects//
 var weapon = {
-    donut: {
-        name: 'donut',
-        power: 10,
-        //image: '<img src="img/donut.jpg" alt="donut">'
-        image: './img/donut.jpg'
-    },
-    sword: {
-        name: 'sword',
-        power: 20,
-        //image: '<img src="img/sword.jpg" alt="sword">'
-        image: './img/sword.jpg'
-    },
-    computer: {
-        name: 'computer',
-        power: 25,
-        //image: '<img src="img/computer.jpg" alt="computer">'
-        image: './img/computer.jpg'
-    },
-    wizard: {
-        name: 'wizard',
-        power: 50,
-        //image: '<img src="img/wizard.jpg" alt="wizard">'
-        image: './img/wizard.jpg'
-    },
+  donut: {
+      name: 'donut',
+      power: 10,
+      //image: '<img src="img/donut.jpg" alt="donut">'
+      image: './img/donut.jpg'
+  },
+  sword: {
+      name: 'sword',
+      power: 20,
+      //image: '<img src="img/sword.jpg" alt="sword">'
+      image: './img/sword.jpg'
+  },
+  computer: {
+      name: 'computer',
+      power: 25,
+      //image: '<img src="img/computer.jpg" alt="computer">'
+      image: './img/computer.jpg'
+  },
+  wizard: {
+      name: 'wizard',
+      power: 25,
+      //image: '<img src="img/wizard.jpg" alt="wizard">'
+      image: './img/wizard.jpg'
+  },
 };
 
 
 function placeTree(){
-  for(let i = 0; i < 12; i++) {
-    let row = randomRowColumnNumber(9);
-    let col = randomRowColumnNumber(9);
-    $(`[data-col="${col}"][data-row="${row}"]`).addClass('full').css({'width': '60px', 'height': '60px', 'background-image': 'url("img/tree.jpg")'});
-    //$(`[data-col="${col}"][data-row="${row}"]`).addClass('full').html('<img src="img/tree.jpg" alt="tree">').css({'width': '60px', 'height': '60px'});
-  }
+for(let i = 0; i < 12; i++) {
+  let row = randomRowColumnNumber(9);
+  let col = randomRowColumnNumber(9);
+  $(`[data-col="${col}"][data-row="${row}"]`).addClass('full').css({'width': '60px', 'height': '60px', 'background-image': 'url("img/tree.jpg")'});
+  //$(`[data-col="${col}"][data-row="${row}"]`).addClass('full').html('<img src="img/tree.jpg" alt="tree">').css({'width': '60px', 'height': '60px'});
+}
 }
 // Square that we select need to tell the square what weapon it has//
 function placeWeapon(weaponImage, weaponName, weaponPower){
-  let row = randomRowColumnNumber(9);
-  let col = randomRowColumnNumber(9);
-  let gridItem = $(`[data-col="${col}"][data-row="${row}"]`);
-  if( gridItem.hasClass('full') ) {
-    console.log('square is taken, trying again');
-    return placeWeapon(weaponImage)
-  }
-  console.log('square avaialble, placing item');
-  gridItem.addClass('weapon full').attr('data-name', weaponName).attr('data-power', weaponPower).attr('data-image', weaponImage).css({'background-image': 'url(' + weaponImage + ')', 'background-repeat': 'no-repeat'});
+let row = randomRowColumnNumber(9);
+let col = randomRowColumnNumber(9);
+let gridItem = $(`[data-col="${col}"][data-row="${row}"]`);
+if( gridItem.hasClass('full') ) {
+  
+  return placeWeapon(weaponImage)
+}
+
+gridItem.addClass('weapon full').attr('data-name', weaponName).attr('data-power', weaponPower).attr('data-image', weaponImage).addClass(weaponName);
 
 };
 // PLAYERS //
@@ -90,18 +96,18 @@ function placeWeapon(weaponImage, weaponName, weaponPower){
 //4.Add the picture//
 function placePlayer(player){
 
-  let row = randomRowColumnNumber(9);
-  let col = randomRowColumnNumber(9);
-  let gridItem = $(`[data-col="${col}"][data-row="${row}"]`);
-  if( gridItem.hasClass('full') ) {
-    console.log('square is taken, trying again');
-    return placePlayer(player);
-  }
-  console.log('square avaialble, placing item');
-  gridItem.addClass('full').html(player.playerImage);
-  player.playerPos.row = row;
-  player.playerPos.col = col;
-  console.log('player position:', player.playerPos);
+let row = randomRowColumnNumber(9);
+let col = randomRowColumnNumber(9);
+let gridItem = $(`[data-col="${col}"][data-row="${row}"]`);
+if( gridItem.hasClass('full') ) {
+ 
+  return placePlayer(player);
+}
+
+gridItem.addClass('full').addClass(player.playerType);
+player.playerPos.row = row;
+player.playerPos.col = col;
+
 
 };
 
@@ -112,18 +118,18 @@ function placePlayer(player){
 //4.Check each of the directions//;
 
 function isWithinRange(playerCol, playerRow, squareCol, squareRow){
-  let colDifference = Math.abs(playerCol - squareCol);
-  let rowDifference = Math.abs(playerRow - squareRow);
+let colDifference = Math.abs(playerCol - squareCol);
+let rowDifference = Math.abs(playerRow - squareRow);
 
-  if(  (colDifference <= 3 && rowDifference === 0) || (rowDifference <= 3 && colDifference === 0)  ){
-    return true;
-  }
-    return false;
+if(  (colDifference <= 3 && rowDifference === 0) || (rowDifference <= 3 && colDifference === 0)  ){
+  return true;
+}
+  return false;
 }
 
 
 
-  //Weapon Check//
+//Weapon Check//
 //1.Get players square clicked//
 //2.Check if clicked square have a class of weapon//
 //3.If it does switch the weapon in activePlayer object, change name and attack//;
@@ -131,40 +137,44 @@ function isWithinRange(playerCol, playerRow, squareCol, squareRow){
 //5.add and remove images(swap weapon images)//;
 
 function pickUpWeapon($this){
+let squareWeaponName = $this.data('name');
+let squareWeaponImage = $this.data('image');
+let squareWeaponPower = $this.data('power');
+let weaponImage = activePlayer.weapon.weaponImage;
+let weaponDamage = activePlayer.weapon.damage;
+let weapon = activePlayer.weapon.weaponName;
 
-  let weaponImage = activePlayer.weaponImage;
-  let weaponDamage = activePlayer.damage;
-  let weapon = activePlayer.weapon;
-  console.log('Hey you are working');
-  activePlayer.weapon = $this.data('name');
-  activePlayer.weaponImage = $this.data('image');
-  activePlayer.damage = $this.data('power');
-  //$(`.${activePlayer.playerType}_weapon`).html($this.data('image'));
-  $(`.${activePlayer.playerType}_weapon`).css({'background-image': 'url(' + $this.data("image") + ')', 'width': '60px', 'height': '60px'});
-  $(`.${activePlayer.playerType}_attack`).html(activePlayer.damage);
-  console.log($this.data('name'));
-  
-  $this.data('name', weapon);
-  $this.data('image', weaponImage);
-  $this.data('power', weaponDamage);
- // $this.css({'background-image': 'url(' + $this.data("image") + ')', 'width': '60px', 'height': '60px'});
-  
+activePlayer.weapon.weaponName = $this.data('name');
+activePlayer.weapon.weaponImage = $this.data('image');
+activePlayer.weapon.damage = $this.data('power');
+
+$this.removeClass(squareWeaponName).addClass(weapon);
+
+
+$this.data('name', weapon);
+$this.data('image', weaponImage);
+$this.data('power', weaponDamage);
+
+$(`.${activePlayer.playerType}_weapon`).addClass(weapon); 
+$(`.${activePlayer.playerType}_attack`).html(squareWeaponPower);
+
+
 
 };
 
 //1. If payers current square has weapon in data-image//
 //2.Then replace image in the div when player moves out of the square//
-function dropOldWeapon(playerCol, playerRow){
-  let currentSquare = $(`[data-col="${playerCol}"][data-row="${playerRow}"]`).data('data-image');
-  
-  console.log(currentSquare);
-  if(currentSquare){
-    return currentSquare;
-  } else {
-    return false;
-  }
-        
-}
+// function dropOldWeapon(playerCol, playerRow){
+//   let currentSquare = $(`[data-col="${playerCol}"][data-row="${playerRow}"]`).data('data-image');
+
+//   console.log(currentSquare);
+//   if(currentSquare){
+//     return currentSquare;
+//   } else {
+//     return false;
+//   }
+      
+// }
 
 
 //Player Movements working with activePlayer//
@@ -174,44 +184,44 @@ function dropOldWeapon(playerCol, playerRow){
 //4.image appears on new square//
 
 function movePlayer($this){
-  let playerCol = activePlayer.playerPos.col;
-  let playerRow = activePlayer.playerPos.row;
-  let squareCol = $this.attr('data-col');
-  let squareRow = $this.attr('data-row');
-  let inRightRange = isWithinRange(playerCol, playerRow, squareCol, squareRow);
- 
+let playerCol = activePlayer.playerPos.col;
+let playerRow = activePlayer.playerPos.row;
+let squareCol = $this.attr('data-col');
+let squareRow = $this.attr('data-row');
+let inRightRange = isWithinRange(playerCol, playerRow, squareCol, squareRow);
+
+
+
+if(inRightRange){
   
-
-  if(inRightRange){
+  // let oldWeapon = dropOldWeapon(playerCol, playerRow);
+  // if(oldWeapon){
+  //   $(`[data-col="${playerCol}"][data-row="${playerRow}"]`).html(activePlayer.weaponImage).removeClass('full');
+  // };
+  if($this.hasClass('weapon')){
+    pickUpWeapon($this);
     
-    let oldWeapon = dropOldWeapon(playerCol, playerRow);
-    if(oldWeapon){
-      $(`[data-col="${playerCol}"][data-row="${playerRow}"]`).html(activePlayer.weaponImage).removeClass('full');
-    };
-    if($this.hasClass('weapon')){
-      pickUpWeapon($this);
-     
-    }
-    
-    $(`[data-col="${playerCol}"][data-row="${playerRow}"]`).html('').removeClass('full');
-    activePlayer.playerPos.row = squareRow;
-    activePlayer.playerPos.col = squareCol;
-    $(`[data-col="${squareCol}"][data-row="${squareRow}"]`).html(activePlayer.playerImage).addClass('full');
-
-  } else{
-    console.log('You can not move more than 3 squares');
   }
- 
+  
+  $(`[data-col="${playerCol}"][data-row="${playerRow}"]`).removeClass(activePlayer.playerType).removeClass('full');
+  activePlayer.playerPos.row = squareRow;
+  activePlayer.playerPos.col = squareCol;
+  $(`[data-col="${squareCol}"][data-row="${squareRow}"]`).addClass(activePlayer.playerType).addClass('full');
+
+} else{
+  console.log('You can not move more than 3 squares');
+}
+
 };
 
 
 
 
 $('.grid-item').on('click', function(){
-  let $this = $(this);
-  //console.log('square clicked', $(this), this );
-  movePlayer($this);
-  
+let $this = $(this);
+//console.log('square clicked', $(this), this );
+movePlayer($this);
+
 });
 
 
